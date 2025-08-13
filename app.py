ed8c1761e3de42556df052d46e35a5ce8712a186
42kitrun <42kit.run@gmail.com>
2025-08-13 12:02:53 +0900

    add : database and added routes

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-app.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/on-map')
+def browse_on_map():
+    return render_template('index.html')
+
+@app.route('/detail/<id>')
+def detail():
+    return render_template('index.html')
+
+# --------------------------------------------
+
+@app.route('/')
+def index():
+    return jsonify("message":"not yet")
+
+@app.route('/')
+def index():
+    return jsonify("message":"not yet")
+
+@app.route('/')
+def index():
+    return jsonify("message":"not yet")
 
 if __name__ == '__main__':
     app.run(debug=True)
