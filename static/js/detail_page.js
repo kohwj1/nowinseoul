42d4e268fab037f343b330e45fdaca217274e001
John Lee <ohno0601111@gmail.com>
2025-08-19 18:31:55 +0900

    read more 버튼 오작동 해결

--- static/js/detail_page.js
+++ static/js/detail_page.js
@@ -2,16 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const textContainer = document.querySelector('.text-container');
     const readMoreBtn = document.getElementById('read-more-btn');
 
-    // 모바일 환경에서만 실행
-    if (window.innerWidth <= 768) {
-        readMoreBtn.addEventListener('click', () => {
-            textContainer.classList.toggle('expanded');
+    // The event listener is now active for all screen sizes
+    readMoreBtn.addEventListener('click', () => {
+        textContainer.classList.toggle('expanded');
 
-            if (textContainer.classList.contains('expanded')) {
-                readMoreBtn.textContent = 'Read less';
-            } else {
-                readMoreBtn.textContent = 'Read more';
-            }
-        });
-    }
+        if (textContainer.classList.contains('expanded')) {
+            readMoreBtn.textContent = 'Read less';
+        } else {
+            readMoreBtn.textContent = 'Read more';
+        }
+    });
 });
