5d6b81700974729b5efc51ce0523c5d7b2915ce8
42kitrun <42kit.run@gmail.com>
2025-08-21 23:06:51 +0900

    추가 : 실시간 인구밀도 스케줄러를 위한 폴더 및 파일 구조화

---
+++ init_app.py
@@ -0,0 +1,13 @@
+ # 앱 초기 설치 및 셋업 함수 모음
+
+from models import init_db
+from services import bike_station_init
+
+def init_app():
+    # create_db_file()
+    # create_tables()
+    # insert_initial_data()
+    print("초기화 완료")
+
+if __name__ == "__main__":
+    init_app()
