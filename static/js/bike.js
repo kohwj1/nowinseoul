56fdf3ddf6f4fea1641f980c66b42f09fff3f708
Arkeil <kohwj1@gmail.com>
2025-08-21 21:15:40 +0900

    수정: bikeList 코드 컨벤션  수정

--- static/js/bike.js
+++ static/js/bike.js
@@ -1,17 +1,18 @@
 
-//따릉이용 마커
+//따릉이용 서클 마커
 function circleOnMap(marker_data, map) {
     try {
         for (m of marker_data.SBIKE_STTS) {
             if (m.SBIKE_PARKING_CNT != 0) {
                 L.circleMarker([m.SBIKE_X, m.SBIKE_Y], {radius: 15, color: '#33a758', fillColor: '#33a758', fillOpacity: 1})
                 .addTo(map)
-                L.marker([m.SBIKE_X, m.SBIKE_Y],{icon: L.divIcon({html: `<div class="circle-text">${m.SBIKE_PARKING_CNT}</div>`, iconSize: [0, 0], iconAnchor: [6, 11],})})
+                L.marker([m.SBIKE_X, m.SBIKE_Y],{icon: L.divIcon({html: `<div class="circle-text">${m.SBIKE_PARKING_CNT}</div>`, iconSize: [0, 0], iconAnchor: [0, 0],})})
                 .bindPopup(`<strong>${m.SBIKE_SPOT_NM}</strong><br><br>${m.SBIKE_PARKING_CNT} bike(s)`)
                 .addTo(map)
         }
     }
     } catch {
+        console.log('서버 데이터 확인 필요')
         return;   
     }
 }
@@ -20,12 +21,12 @@ function circleOnMap(marker_data, map) {
 document.addEventListener('DOMContentLoaded', () => {
     //지도 렌더링
     try{
-        const init_lat = bike_list.SBIKE_STTS[0].SBIKE_X;
-        const init_lng = bike_list.SBIKE_STTS[0].SBIKE_Y;
-        console.log(init_lat, init_lng);
+        const initLat = bikeList.SBIKE_STTS[0].SBIKE_X;
+        const initLng = bikeList.SBIKE_STTS[0].SBIKE_Y;
+        console.log(initLat, initLng);
         
         let map = L.map('map', {
-            center: [init_lat, init_lng],
+            center: [initLat, initLng],
             zoom: 16,
         });
         
@@ -33,18 +34,11 @@ document.addEventListener('DOMContentLoaded', () => {
             maxZoom: 17,
             attribution: '&copy; OpenStreetMap contributors'
         }).addTo(map);
-        circleOnMap(bike_list, map)
+        circleOnMap(bikeList, map)
     } catch {
-        let map = L.map('map', {
-            center: [37.5665, 126.9780],
-            zoom: 16,
-        });
-        
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 17,
-            attribution: '&copy; OpenStreetMap contributors'
-        }).addTo(map);
-        circleOnMap(bike_list, map)
+        const msgNoBike = document.createElement('div')
+        msgNoBike.textContent = 'No bike available'
+        msgNoBike.classList.add('no-bike')
+        document.getElementById('map').appendChild(msgNoBike)
     }
-
 })
