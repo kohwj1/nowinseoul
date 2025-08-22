54378d7929a83a11198086949cd0fc9daf5790cc
42kitrun <42kit.run@gmail.com>
2025-08-22 13:57:53 +0900

    수정 : thumbnail 컬럼 제거

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ app.config['DATABASE'] = os.path.join(app.instance_path, 'nowinseoul.db')
 @app.route('/')
 def index():
     return render_template('index.html', data = [
-            {'id':'POI095','thumnail':'POI095.jpg','name': 'Banpo Hangang Park'},
-            {'id':'POI096','thumnail':'POI096.png','name': 'Dream Forest'},
-            {'id':'POI098','thumnail':'POI098.jpg','name': 'Seoripul Park·Montmartre Park'},
-            {'id':'POI099','thumnail':'POI099.jpg','name': 'Seoul Plaza'},
-            {'id':'POI100','thumnail':'POI100.png','name': 'Seoul Grand Park'},
-            {'id':'POI101','thumnail':'POI101.png','name': 'Seoul Forest'},
+            {'id':'POI095','name': 'Banpo Hangang Park'},
+            {'id':'POI096','name': 'Dream Forest'},
+            {'id':'POI098','name': 'Seoripul Park·Montmartre Park'},
+            {'id':'POI099','name': 'Seoul Plaza'},
+            {'id':'POI100','name': 'Seoul Grand Park'},
+            {'id':'POI101','name': 'Seoul Forest'},
         ])
 
 # 지도 페이지
@@ -220,12 +220,12 @@ def detail(id):
 def filter_by_tags():
     return render_template('index.html', data = {"tags" : ["food", "movie"],
         "data":[
-            {'id':'POI095','thumnail':'POI095.jpg','name': 'Banpo Hangang Park'},
-            {'id':'POI096','thumnail':'POI096.png','name': 'Dream Forest'},
-            {'id':'POI098','thumnail':'POI098.jpg','name': 'Seoripul Park·Montmartre Park'},
-            {'id':'POI099','thumnail':'POI099.jpg','name': 'Seoul Plaza'},
-            {'id':'POI100','thumnail':'POI100.png','name': 'Seoul Grand Park'},
-            {'id':'POI101','thumnail':'POI101.png','name': 'Seoul Forest'},
+            {'id':'POI095','name': 'Banpo Hangang Park'},
+            {'id':'POI096','name': 'Dream Forest'},
+            {'id':'POI098','name': 'Seoripul Park·Montmartre Park'},
+            {'id':'POI099','name': 'Seoul Plaza'},
+            {'id':'POI100','name': 'Seoul Grand Park'},
+            {'id':'POI101','name': 'Seoul Forest'},
         ]
     })
 
