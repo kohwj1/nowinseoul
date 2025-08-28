9d7cdcc5f33999eb6910f760c1caabb2539c401f
Arkeil <kohwj1@gmail.com>
2025-08-28 16:20:51 +0900

    추가: 지도 페이지 다국어 대응

---
+++ static/js/translate_js.js
@@ -0,0 +1,52 @@
+function translateToast(language, text) {
+    const trans_data = {
+        'ko': {
+                'No Search Result':'검색 결과가 없습니다',
+                'Please enter a keyword':'검색어를 입력해주세요',
+            },
+        'ja': {
+                'No Search Result':'検索結果がありません',
+                'Please enter a keyword':'検索ワードを入力してください',
+            },
+        'en': {
+                'No Search Result':'No Search Result',
+                'Please enter a keyword':'Please enter a keyword',
+            },
+    }
+
+    if (trans_data.hasOwnProperty(language)) {
+        return trans_data[language][text];
+    }
+    return trans_data['en'][text];
+
+}
+
+function translateCrowd(language, text) {
+    const trans_data = {
+        'ko': {
+                'Comfortable':'여유',
+                'Moderate':'보통',
+                'Slightly crowded':'약간 붐빔',
+                'Crowded':'혼잡',
+                'Go to Detail': '자세히 보기'
+            },
+        'ja': {
+                'Comfortable':'余裕',
+                'Moderate':'普通',
+                'Slightly crowded':'少々混雑',
+                'Crowded':'混雑',
+                'Go to Detail': 'もっと見る'
+            },
+        'en': {
+                'Comfortable':'Comfortable',
+                'Moderate':'Moderate',
+                'Slightly crowded':'Slightly crowded',
+                'Crowded':'Crowded',
+                'Go to Detail': 'Go to Detail'
+            },
+    }
+    if (trans_data.hasOwnProperty(language)) {
+        return trans_data[language][text];
+    }
+    return trans_data['en'][text];
+}
